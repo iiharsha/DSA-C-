22a55002de5fbfc6de0aead52c5cf3c25f5e9da7
iiharsha <iiharsha10@gmail.com>
2024-06-10 22:11:46 +0530

    Valid Word Solution added

--- isValidWord.cpp
+++ isValidWord.cpp
@@ -1,3 +1,13 @@
+/*A word is considered valid if:
+
+It contains a minimum of 3 characters.
+It contains only digits (0-9), and English letters (uppercase and lowercase).
+It includes at least one vowel.
+It includes at least one consonant.
+You are given a string word.
+
+Return true if word is valid, otherwise, return false.
+ */
 #include <bits/stdc++.h>
 using namespace std;
 
